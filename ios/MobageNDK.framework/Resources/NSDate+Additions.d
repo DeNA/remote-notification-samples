d49358ec0131ae9db040ac03ffa1ce7f4842f9ce
Ian Terrell <iterrell@ngmoco.com>
2012-11-28 10:01:25 -0800

    moved around NDK and cleaned up build

--- ios/MobageNDK.framework/Resources/NSDate+Additions.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hwang/ngmoco/penguin/ios/US/../mobage-ndk/mobage-ndk/Utilities/NSDate+Additions.m \
-  /Users/hwang/ngmoco/penguin/ios/US/../mobage-ndk/mobage-ndk/Utilities/NSDate+Additions.h
